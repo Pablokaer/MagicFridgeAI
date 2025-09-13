198e7fca22b0a55f1b4f04bf58f215d9b43ad677
Pablo Carvalho <pablo.luan.carvalho@gmail.com>
2025-09-13 01:30:44 +0100

    Warning! We should work on our GeminiService class, it isn't working.

--- src/main/java/dev/java10x/MagicFridgeAI/controller/RecipeController.java
+++ src/main/java/dev/java10x/MagicFridgeAI/controller/RecipeController.java
@@ -1,7 +1,26 @@
 package dev.java10x.MagicFridgeAI.controller;
 
+import dev.java10x.MagicFridgeAI.model.FoodItem;
+import dev.java10x.MagicFridgeAI.service.GeminiService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class RecipeController {
+
+    private GeminiService geminiService;
+
+    public RecipeController(GeminiService geminiService) {
+        this.geminiService = geminiService;
+    }
+
+    @GetMapping("/generate")
+    public Mono<ResponseEntity<String>> generateRecipe(){
+        return geminiService.generateRecipe()
+        .map(recipe -> ResponseEntity.ok(recipe))
+                .defaultIfEmpty(ResponseEntity.noContent().build());
+    }
+
 }
